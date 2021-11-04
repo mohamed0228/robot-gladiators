f39ece5075eb277c32f060753bcaf708005c65b3
mohamed0228 <mohamed0228@outlook.fr>
2021-11-03 21:04:30 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,13 +11,26 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 console.log(enemyNames);
+if (playerHealth > 0) {
+  // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+  window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
-for (var i = 0; i < enemyNames.length; i++) {
+  // pick new enemy to fight based on the index of the enemyNames array
   var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
-}
 
+  // reset enemyHealth before starting new fight
+  enemyHealth = 50;
+
+  // use debugger to pause script from running and check what's going on at that moment in the code
+  // debugger;
+
+  // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+  fight(pickedEnemyName);
+}
+else {
+  window.alert("You have lost your robot in battle! Game Over!");
+  break;
+}
 
 // fight function
 var fight = function(enemyName) {
